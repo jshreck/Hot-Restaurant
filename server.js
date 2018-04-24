9f4f212707986c93cc0a60aa204a9394a4c09fb0
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-04-24 19:59:18 -0400

    Committing for sanity

--- server.js
+++ server.js
@@ -10,14 +10,37 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var table = [
-    {
-        id: 0,
-        reservedBy: "Julia"
-    }
-];
+// var table = [
+//     {
+//         id: 0,
+//         reservedBy: "Julia"
+//     }
+// ];
 
 var reservations = [];
+var waitlist = [];
+var tables = [
+    {
+        id: 1,
+        seated: 'Joe'
+    }, 
+    {
+        id: 2,
+        seated: 'Suzy'
+    },
+    {
+        id: 3,
+        seated: 'Sally'
+    },
+    {
+        id: 4,
+        seated: 'Greg'
+    }, 
+    {
+        id: 5,
+        seated: 'Jim'
+    }
+];
 
 app.get("/api/reservations", function (req, res) {
     return res.send(JSON.stringify(reservations));
@@ -25,6 +48,24 @@ app.get("/api/reservations", function (req, res) {
 
 app.post("/api/reservations", function (req,res) {
     var reservation = req.body;
-    reservations.push(reservation)
+    
+    //count = 0;
+    // reservations.forEach(reservation, function () {
+    //     if (count < 5) {
+    //         //seat at tables
+    //     count++;
+    //     }
+
+    //     else {
+    //         //put on waitlist
+    //     }
+    // });
+    
+    if (reservations.length >= 4) {
+        waitlist.push(reservation);
+    }
+    else {
+        reservations.push(reservation)
+    }
 });
 
